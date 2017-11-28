9f460317a59a458d251381feb7a5e86c032be235
Your Name <you@example.com>
2017-11-28 16:42:53 +0800

    '轮播'

---
+++ react-cnr/src/Redux/HomeRedux.js
@@ -0,0 +1,10 @@
+const HomeRedux = (state={}, action) => {
+	switch(action.type){
+		case 'SINGLE_GET_DATA':
+		console.log(action.payload);
+			return action.payload;
+		default:
+			return state;
+	}
+}
+export default HomeRedux;
